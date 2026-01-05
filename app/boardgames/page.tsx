3aedd4d972b6c7f36200b071be4e18e09e436f6b
Erik Börne <erik.borne@outlook.com>
2026-01-05 13:39:18 +0100

    feat: create simple board game table

--- app/boardgames/page.tsx
+++ app/boardgames/page.tsx
@@ -1,11 +1,72 @@
+
+const TableHeader = <tr>
+    <td>Name</td>
+    <td>Players</td>
+    <td>Play Time</td>
+    <td>Year Published</td>
+</tr>
+
+interface BoardGames {
+    name: string;
+    minPlayers: number;
+    maxPlayers: number;
+    minPlayTime: number;
+    maxPlayTime: number;
+    publisher?: string;
+    yearPublished?: number;
+}
+
+const boardGames: BoardGames[] = [
+    {
+        name: "Catan",
+        minPlayers: 3,
+        maxPlayers: 4,
+        minPlayTime: 60,
+        maxPlayTime: 120,
+        publisher: "Kosmos",
+        yearPublished: 1995,
+    },
+    {
+        name: "Pandemic",
+        minPlayers: 2,
+        maxPlayers: 4,
+        minPlayTime: 45,
+        maxPlayTime: 60,
+        publisher: "Z-Man Games",
+        yearPublished: 2008,
+    },
+    {
+        name: "Ticket to Ride",
+        minPlayers: 2,
+        maxPlayers: 5,
+        minPlayTime: 30,
+        maxPlayTime: 60,
+        publisher: "Days of Wonder",
+        yearPublished: 2004,
+    },
+];
+
 export default function BoardGames() {
     return (
-        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-            <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-                <h1 className="text-4xl font-bold text-gray-900 dark:text-white">
-                    Board Games Page
-                </h1>
-            </main>
+        <div className="p-8">
+            <table className="table table-xs table-pin-rows table-pin-cols">
+                <thead>
+                    {TableHeader}
+                </thead>
+                <tbody>
+                    {boardGames.map((game, index) => (
+                        <tr key={index} className="text-lg">
+                            <td>{game.name}</td>
+                            <td>{game.minPlayers} - {game.maxPlayers}</td>
+                            <td>{game.minPlayTime} - {game.maxPlayTime} min</td>
+                            <td>{game.yearPublished ?? "-"}</td>
+                        </tr>
+                    ))}
+                </tbody>
+                <tfoot>
+                    {TableHeader}
+                </tfoot>
+            </table>
         </div>
     );
 }

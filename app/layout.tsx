c12984700d64200b7aca02628796ed644822b58e
Erik Börne <erik.borne@outlook.com>
2026-03-18 23:07:59 +0100

    feat: Add groups and structure to Drawer menu links
    This commit improves the look of the side navigation drawer by grouping 'Board Games', 'Books', and 'Lego' under a 'Collections' header utilizing standard DaisyUI titles. Additionally, it groups 'Users (Admin)' into an 'Administration' block solely for administrative personnel.

--- app/layout.tsx
+++ app/layout.tsx
@@ -83,6 +83,7 @@ export default function RootLayout({
                   Home
                 </DrawerLink>
               </li>
+              <li className="menu-title mt-2">Collections</li>
               <li>
                 <DrawerLink href="/boardgames">
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 opacity-70" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
